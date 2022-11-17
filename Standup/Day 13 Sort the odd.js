749a4bc6dd865043dabbb2d7f5bfd302da2e7fea
Caleb Cohen <CalebCohen@hotmail.com>
2022-11-17 13:40:40 -0600

    fix

--- Standup/Day 13 Sort the odd.js
+++ Standup/Day 13 Sort the odd.js
@@ -1,4 +1,4 @@
-https://www.codewars.com/kata/578aa45ee9fd15ff4600090d
+//https://www.codewars.com/kata/578aa45ee9fd15ff4600090d
 
 // will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
 
@@ -16,4 +16,4 @@ function sortArray(array){
 }
 
 console.log(sortArray([7, 1]), [1, 7])
-console.log(sortArray([5, 8, 6, 3, 4]), [3, 8, 6, 5, 4] )
+console.log(sortArray([5, 8, 6, 3, 4]), [3, 8, 6, 5, 4])
